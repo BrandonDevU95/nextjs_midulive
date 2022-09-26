03bdbe300391c62b7bda11ed02126ffc2d2b643a
BrandonDevU95 <brandon.vargas1095@hotmail.com>
2022-09-26 10:16:16 -0500

    Article

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,4 +1,3 @@
-import { Container, Text } from "@nextui-org/react";
 import Link from "next/link";
 import React from "react";
 
@@ -6,7 +5,11 @@ export default function Header() {
    return (
       <header className="flex justify-between items-center p-4 max-w-xl m-auto">
          <h1 className="font-bold">
-            Next <span className="font-light">xkcd</span>
+            <Link href="/">
+               <a className="transition hover:opacity-80">
+                  Next <span className="font-light">xkcd</span>
+               </a>
+            </Link>
          </h1>
          <nav>
             <ul className="flex flex-row gap-2">
